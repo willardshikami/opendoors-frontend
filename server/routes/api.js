80106e75e3bbf0399ea6514aba31e006d3e4f784
willardshikami <willardshikami@gmail.com>
2018-07-09 14:25:50 +0300

    connected to mlab using mongoose

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
 
-const app = express();
+const db = "mongodb://od:shikami1@ds261540.mlab.com:61540/opendoorsjobs";
+
+//mongoose connection string 
+mongoose.connect(db, {useNewUrlParser: true }, err =>{
+  if (err){
+    console.error('Error ' + err);
+  }else{
+    console.log('Connected to DB')
+  }
+})
 
 router.get('/', (req, res) => {
   res.send('API')
