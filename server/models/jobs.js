cdfd6c91773fb3749abfa93a57219049f1833d22
willardshikami <willardshikami@gmail.com>
2018-07-10 15:40:18 +0300

    modified jobs model

--- server/models/jobs.js
+++ server/models/jobs.js
@@ -2,30 +2,41 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const TechStack = new Schema({
+  name: String
+});
+
+const Qualifications = new Schema({
+  name: String
+})
+
+const BonusSkills = new Schema({
+  name: String
+});
+
+const Responsibilities = new Schema({
+  name: String
+})
+
+const Benefits = new Schema({
+  name: String
+})
+
 const jobSchema = new Schema({
   job_title: String,
   company_name: String,
   company_log: String,
   about_company: String,
   location: String,
-  tech_stack: {
-    type: [String]
-  },
-  qualifications: {
-    type: [String]
-  },
-  bonus_skills: {
-    type: [String]
-  },
-  responsibilities: {
-    type: [String]
-  },
-  benefits: {
-    type: [String]
-  },
+  tech_stack: [TechStack],
+  qualifications: [Qualifications],
+  bonus_skills: [BonusSkills],
+  responsibilities: [Responsibilities],
+  benefits: [Benefits],
   apply: String,
   date_posted: String,
   deadline: String
 })
+
 
 module.exports = mongoose.model('job_list', jobSchema, 'jobs')
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Job = require('../models/jobs');
 
+//mlab connection string
 const db = "mongodb://od:shikami1@ds261540.mlab.com:61540/opendoorsjobs";
 
 //mongoose connection string 
@@ -16,5 +18,21 @@ mongoose.connect(db, {useNewUrlParser: true }, err =>{
 router.get('/', (req, res) => {
   res.send('API')
 });
+
+
+//submit jobs
+router.post('/submit', (req, res) => {
+  let jobData = req.body;
+  let job = new Job(jobData)
+  job.save((error, submitedJob) => {
+    if(error){
+      console.log(error)
+    }else{
+      res.status(200).send(submitedJob);
+    }
+  });
+})
+
+
 
 module.exports = router;
